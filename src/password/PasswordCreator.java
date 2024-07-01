a30a8e9b042fdfb594eecf2037187ae97544c13b
LindauroNeto <140420389+LindauroNeto@users.noreply.github.com>
2024-07-01 20:03:50 -0300

    Ajustando o 'ranNumber'

--- src/password/PasswordCreator.java
+++ src/password/PasswordCreator.java
@@ -6,7 +6,9 @@ import java.util.Random;
 public class PasswordCreator {
 	private static ArrayList<String> organizador = new ArrayList<String>();
 	private static Random aleatorio = new Random();
-	private static char ranNumber;
+	private static char ranNumber(String opcao) {
+		return opcao.charAt(aleatorio.nextInt(opcao.length()));
+	};
 	// v Insipirado pela Amannitta!!! Não foi feito originalmente por mim!!!!!
 	// Confira o repositório dela: https://github.com/Amannitta/Gerador-de-Senhas
 	private static String opcoesGeral = "abcçdefghijklmnopqrstuvwxyzABCÇDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$_%&?*§=+¢/\\";
@@ -16,18 +18,15 @@ public class PasswordCreator {
 	public static void geradorSenha(int qnt) {
 		if (qnt > 5) {
 			for (int i = 0; i < qnt; i++) {
-				ranNumber = opcoesLetra.charAt(aleatorio.nextInt(opcoesLetra.length()));
-				organizador.add(Character.toString(ranNumber));
+				organizador.add(Character.toString(ranNumber(opcoesLetra)));
 			}
 
 			for (int i = 0; i <= qnt / 3; i++) {
-				ranNumber = opcoesMais.charAt(aleatorio.nextInt(opcoesMais.length()));
-				organizador.set(aleatorio.nextInt(organizador.size() - 1), Character.toString(ranNumber));
+				organizador.set(aleatorio.nextInt(organizador.size() - 1), Character.toString(ranNumber(opcoesMais)));
 			}
 		} else {
 			for (int i = 0; i < qnt; i++) {
-				ranNumber = opcoesGeral.charAt(aleatorio.nextInt(opcoesGeral.length()));
-				organizador.add(Character.toString(ranNumber));
+				organizador.add(Character.toString(ranNumber(opcoesGeral)));
 			}
 		}
 
